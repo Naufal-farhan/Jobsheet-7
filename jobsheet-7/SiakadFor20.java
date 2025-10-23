fb4e5b882712c3ebf4097702dce4a9e1ee77946f
Naufal-farhan <nefener123@gmail.com>
2025-10-23 11:46:50 +0700

    Modifikasi percobaan-1

--- jobsheet-7/SiakadFor20.java
+++ jobsheet-7/SiakadFor20.java
@@ -7,7 +7,8 @@ public class SiakadFor20{
         double nilai;
         double terendah = 100;
         double tertinggi = 0;
-
+        int lulus = 0;
+        int Tlulus = 0;
         //kode program (for)
         for (int i = 1;i <= 10; i++){
             System.out.print("Masukkan nilai mahasiswa ke-" + i + " : ");
@@ -19,8 +20,16 @@ public class SiakadFor20{
             if (nilai < terendah){
                 terendah = nilai;
             }
+            if (nilai >= 60){
+                lulus++;
+            }
+            else {
+                Tlulus++;
+            }
         }
         System.out.println("Tertinggi adalah : " + tertinggi);
         System.out.println("Terendah adalah = " + terendah);
+        System.out.println("Mahasiswa lulus : "+ lulus);
+        System.out.println("Mahasiswa tidak lulus : " + Tlulus);
     }
 }
