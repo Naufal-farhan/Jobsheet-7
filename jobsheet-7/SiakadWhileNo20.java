aefde1a7e20c098b6a46de0b600a411a1c4800cd
Naufal-farhan <nefener123@gmail.com>
2025-10-23 12:17:08 +0700

    percobaan-2

---
+++ jobsheet-7/SiakadWhileNo20.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class SiakadWhileNo20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //deklarasi scanner
+        int i = 0, jml, nilai;
+        System.out.print("Input jumlah mahasiswa : ");
+        jml = sc.nextInt();
+
+        while (i < jml){
+            System.out.print("Input Nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilai = sc.nextInt();
+
+            if (nilai <0 || nilai >= 100){
+                System.out.println("Nilai tidak valid, silahkan masukkan nilai yang valid");
+                continue;
+            }
+            if (nilai > 80 && nilai <= 100){
+                System.out.println("Nilai mahasiswa ke-"+ (i + 1) +" Adalah A ");
+            }   else if (nilai > 73 && nilai <=80){
+                System.out.print("Nilai mahasiswa ke-"+ (i + 1) +" Adalah B+ ");
+            }   else if (nilai > 65 && nilai <=73){
+                System.out.println("Nilai mahasiswa ke-"+ (i + 1) +" Adalah B ");
+            }   else if (nilai > 60 && nilai <=65){
+                System.out.println("Nilai mahasiswa ke-"+ (i + 1) +" Adalah C+ ");
+            }   else if (nilai > 50 && nilai <=60){
+                System.out.println("Nilai mahasiswa ke-"+ (i + 1) +" Adalah C ");
+            }   else if (nilai > 39 && nilai <=50){
+                System.out.print("Nilai mahasiswa ke-"+ (i + 1) +" Adalah D ");
+            }   else{
+                System.out.println("Nilai mahasiswa ke-"+ (i + 1) +" Adalah E "); 
+            }
+            i++;
+        }
+    }
+    
+}
